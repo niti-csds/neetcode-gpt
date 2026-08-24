c284ed478c0491ca58d672eb8ddc663193440135
niti-csds <jainniti347@gmail.com>
2026-08-24 23:49:47 +0530

    Implement Gradient descent optimizer (Math Foundations 1/6)

--- foundations/gradient_descent.py
+++ foundations/gradient_descent.py
@@ -4,14 +4,10 @@ class Solution:
         # Derivative:         f'(x) = 2x
         # Update rule:        x = x - learning_rate * f'(x)
         # Round final answer to 5 decimal places
+        minimizer = init
+        while iterations > 0:
+            deri = 2*minimizer
+            minimizer = minimizer - learning_rate*deri
+            iterations -= 1
+        return round(minimizer,5)
         pass
-        i = 0
-        x = init
-        
-        while i < iterations:
-            derivative = 2*x
-            x_new = x - learning_rate*(derivative)
-            x = x_new
-            i += 1 
-        return round(x, 5)
-
